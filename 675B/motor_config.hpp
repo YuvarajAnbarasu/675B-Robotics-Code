7a168936a3be1d691381ca4dca4892ae8cf3fa46
YuvarajAnbarasu <107527374+YuvarajAnbarasu@users.noreply.github.com>
2023-10-07 13:39:58 -0400

    Add files via upload

---
+++ 675B/motor_config.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include "api.h"
+
+extern pros:: Motor intake;
+
+extern pros:: Motor catapult;
+
+extern pros:: ADIDigitalOut pistonWingA;
+extern pros:: ADIDigitalOut pistonWingB;
+
+extern pros:: ADIDigitalIn LSwitch;
---
+++ 675B/op_control.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "api.h"
+
+void op_control();
+
+void intake_control();
+void cata_control();
+void wing_control();
