7bba1119ddea35e1002a564e6700313e6243eb29
YuvarajAnbarasu <107527374+YuvarajAnbarasu@users.noreply.github.com>
2023-10-08 22:57:52 -0400

    Update op_control.cpp

--- src/op_control.cpp
+++ src/op_control.cpp
@@ -30,6 +30,43 @@ void cata_control(){
     catapult.move_velocity(0);
 }
 
+void cata_shoot(){
+    if(master.get_digital(pros::E_CONTROLLER_DIGITAL_X)){
+        catapult.move_velocity(-150);
+        pros::delay(10);
+    }
+}
+
+void skills_cata() {
+
+    bool shooting = false;  
+
+    while (true) {
+        if (master.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+            if (!shooting) {  
+                shooting = true;
+                catapult.move_velocity(-150);  
+            } else {
+                shooting = false;
+                catapult.move_velocity(0);  
+            }
+
+            while (master.get_digital(pros::E_CONTROLLER_DIGITAL_B)) {
+                pros::delay(10);  
+            }
+        }
+
+    }
+}
+
+void cata_sit(){
+    if(master.get_digital(pros::E_CONTROLLER_DIGITAL_Y)){
+        catapult.move_velocity(-150);
+        pros::delay(100);
+        catapult.move_voltage(0);
+    }
+}
+
 void wing_control(){
     if(master.get_digital(pros::E_CONTROLLER_DIGITAL_A)){
         pistonWingA.set_value(true);
